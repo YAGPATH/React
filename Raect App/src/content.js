511428402b17d5b483fc409f7b85666e401d4740
yagpath <vikasm027@gmail.com>
2023-07-18 21:46:05 +0530

    react list and key topic completed

--- Raect App/src/content.js
+++ Raect App/src/content.js
@@ -1,34 +1,81 @@
 
 import { useState } from 'react';
+import { FaTrashAlt} from 'react-icons/fa';
 
 const Content = () => {
-  const [name, setName] = useState("Let's Start");
-  const [count, setCount] = useState(0);
- 
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Mivi Earpod",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Boat Sound Music",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Gigabyte PC",
+    },
+    {
+      id: 4,
+      checked: false,
+      item: "Antec Esport",
+    },
+  ]);
+  
+ const handleCheck = (id) => {
+  const listItems = items.map((item) => item.id === id ? {...item, 
+  checked: !item.checked}: item);
 
-    let handleNameChange = () => {
-      let names = ["vikas", "Rajan", "Akku"];
-      let int = Math.floor(Math.random() * 3);
-      setName (names[int]);
-    };
+  setItems(listItems);
+  localStorage.setItem('shoppingList', JSON.stringify(listItems));
+ 
+ }
+
+ const handleDelete = (id) => {
+    console.log(id);
+
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+
+
+ }
 
     
-
-
-    let handleCount = () => {
-      setCount(count+1);
-      console.log(count);
-    };
-
     
     
   return (
     <main>
-      <p>{name}!</p>
 
-      <button onClick={handleNameChange}>Change Name</button>
+      {items.length ? (
+      
+        <ul>
+          {items.map((item) => (
+            <li className='item' key = {item.id}>
+              <input
+                type = 'checkbox'
+                onChange = {() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                onDoubleClick={() => handleCheck(item.id)}
+              
+              > {item.item} </label>
 
-      <button onClick={handleCount}>Count</button>
+              <FaTrashAlt
+              onClick={() => handleDelete(item.id)} role = "button" tabindex ="0"/>
+
+            </li>
+          ))}
+        </ul>
+
+      ) : (
+        <p style=  {{marginTop: '2rem'}}> Your List Is Empty</p>
+      )}
     </main>
   );
 };
